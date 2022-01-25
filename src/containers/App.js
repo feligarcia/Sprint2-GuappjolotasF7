5f8cc4854e294e2cc36de20ffb0985f70f22360c
Juan Felipe Garcia <jufeg2@gmail.com>
2022-01-24 23:23:47 -0500

    estructura de styled

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import Carrito from "./Carrito";
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='App'>
+      
+      <Carrito />
+      
+    </div>
+  );
+}
+
+export default App;
