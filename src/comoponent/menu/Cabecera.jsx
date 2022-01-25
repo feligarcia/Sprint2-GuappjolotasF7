503ce3ed6a5ab9228acad2fdf0c8a4efa083456f
Xiomara Garcia <xiomg1570@gmail.com>
2022-01-25 00:45:01 -0500

    Primeros pasos en la ventana principal

--- src/comoponent/menu/Cabecera.jsx
+++ src/comoponent/menu/Cabecera.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import Logo from "../../assets/images/logo.png"
+import Logo from "../../assets/images/logo.png";
+import { Flexrow, Pprodut, Search, SearchInput } from '../../styles/Styles';
 
 export default class Cabecera extends Component {
   render() {
-    return <div>
-        <div>
-            <img src={Logo} alt="Logo" />
-        </div>
-    </div>;
+    return (<div className='cabecera'>
+        <Flexrow>
+
+            <div className='cont-image'>
+            <img className='image'
+             src={Logo} alt="Logo" />
+            </div>
+
+            <i className="bi bi-cart2"></i>
+        </Flexrow>
+        <p>Nada como una guajolotas para empezar el día</p>
+        <Search>
+            <i className="bi bi-search"></i>
+            <SearchInput type="text" name='producto'
+             placeholder='Sabor de guajolota, bebida...' />
+        </Search>
+        <Flexrow>
+            <Pprodut className='Pproduct'>Guajolotas</Pprodut>
+            <Pprodut className='Pproduct'>Bebidas</Pprodut>
+            <Pprodut className='Pproduct'>Tamales</Pprodut>
+        </Flexrow>
+    </div>);
   }
 }
---
+++ src/styles/Styles.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+
+
+
+export const Flexrow = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 3% 5% 2% 0;
+`
+
+export const Search = styled.div`
+    border-radius: 50px;
+    background-color: #e7e7e7;
+    padding: 5%;
+    width: 80%;
+    height: 10%;
+    margin:auto;
+    display:flex;
+    gap:2%;
+`
+export const SearchInput = styled.input`
+    width:100%;
+    border:none;
+    background-color: #e7e7e7;
+`
+
+export const Pprodut = styled.p`
+    font-size: 1rem;
+    
+`
