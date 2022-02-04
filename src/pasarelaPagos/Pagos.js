1f03ef27715b61186c6d396b54f73e4e0670d93f
Xiomara Garcia <xiomg1570@gmail.com>
2022-02-03 19:51:02 -0500

    pagos

--- src/pasarelaPagos/Pagos.js
+++ src/pasarelaPagos/Pagos.js
@@ -3,7 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 import CheckoutForm from "./CheckoutForm";
-import "./App.css";
+import "./Pagos.css";
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
